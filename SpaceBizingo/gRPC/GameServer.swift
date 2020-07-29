073871d6f39ea8ef6a1e1dece946728767f782dc
GabrielaBezerra <gabrieladecarvalhobezerra@gmail.com>
2020-07-29 12:26:31 -0300

    Now working using local IP

--- SpaceBizingo/gRPC/GameServer.swift
+++ SpaceBizingo/gRPC/GameServer.swift
@@ -15,9 +15,9 @@ import SwiftProtobuf
 class GameServer {
     
     var ip: String {
-        return "localhost"
-        //getIPAddress()!
-        //getGlobalIPAddress()!
+        //"localhost"
+        IPManager.localAddress!
+        //IPManager.globalAddress!
     }
     
     var serverPort: Int = 0
@@ -41,7 +41,7 @@ class GameServer {
         // Start the server and print its address once it has started.
         let server = Server.insecure(group: group)
             .withServiceProviders([provider])
-            .bind(host: "localhost", port: serverPort)
+            .bind(host: ip, port: serverPort)
 
         self.provider = provider
         
@@ -60,49 +60,5 @@ class GameServer {
         // Wait on the server's `onClose` future to stop the program from exiting.
         _ = try? server.flatMap { $0.onClose }.wait()
     }
-    
-    
-//    func getIPAddress() -> String? {
-//           var address: String?
-//           var ifaddr: UnsafeMutablePointer<ifaddrs>?
-//           if getifaddrs(&ifaddr) == 0 {
-//               var ptr = ifaddr
-//               while ptr != nil {
-//                   defer { ptr = ptr?.pointee.ifa_next }
-//                   let interface = ptr?.pointee
-//                   let addrFamily = interface?.ifa_addr.pointee.sa_family
-//                   if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6),
-//                       let cString = interface?.ifa_name,
-//                       String(cString: cString) == "en0",
-//                       let saLen = (interface?.ifa_addr.pointee.sa_len) {
-//                       var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
-//                       let ifaAddr = interface?.ifa_addr
-//                       getnameinfo(ifaAddr,
-//                                   socklen_t(saLen),
-//                                   &hostname,
-//                                   socklen_t(hostname.count),
-//                                   nil,
-//                                   socklen_t(0),
-//                                   NI_NUMERICHOST)
-//                       address = String(cString: hostname)
-//                   }
-//               }
-//               freeifaddrs(ifaddr)
-//           }
-//           return address
-//       }
-//
-//       func getGlobalIPAddress() -> String? {
-//           do {
-//               if let url = URL(string: "https://api.ipify.org") {
-//                   let ipAddress = try String(contentsOf: url)
-//                   print("My public IP address is: " + ipAddress)
-//                   return ipAddress
-//               }
-//           } catch let error {
-//               print(error)
-//           }
-//           return nil
-//       }
     
 }
